b67899beee6434645871a0864090579f09d6cc8d
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2009-09-14 22:24:18 +0800

    Add memcache.

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.db import BadValueError
 from google.appengine.ext.webapp import template
@@ -10,10 +11,13 @@ class Dashboard(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'views/admin.html')
         categories = Category.all().fetch(1000)
+        memcache.set(key='categories', value=categories, time=3600)
         feeds = Feed.all().fetch(1000)
+        memcache.set(key='feeds', value=feeds, time=3600)
         planets = Planet.all().fetch(1000)
+        memcache.set(key='planets', value=planets, time=3600)
         vars = {
-                'title': 'Dashboard - <a href="/">Planet Linux of Taiwan</a>',
+                'title': 'Dashboard - Planet Linux of Taiwan',
                 'categories': categories,
                 'feeds': feeds,
                 'planets': planets,
@@ -23,18 +27,18 @@ class Dashboard(webapp.RequestHandler):
         try:
             type = self.request.get('type')
             action = self.request.get('action')
-            if type == None or action == None:
+            if type is None or action is None:
                 self.redirect('/admin')
             if type == 'planet':
                 name = self.request.get('name')
                 url = self.request.get('url')
-                if name == None or url == None:
+                if name is None or url is None:
                     self.redirect('/admin')
                 planet = Planet(name=name, url=url)
                 planet.put()
             elif type == 'category':
                 name = self.request.get('name')
-                if name == None:
+                if name is None:
                     self.redirect('/admin')
                 category = Category(name=name)
                 category.put()
@@ -42,11 +46,11 @@ class Dashboard(webapp.RequestHandler):
                 name = self.request.get('name')
                 url = self.request.get('url')
                 category = self.request.get('category')
-                if name == None or url == None or category == None:
+                if name is None or url is None or category is None:
                     self.redirect('/admin')
                 query = Category.all()
                 parent = query.filter('name =', category).get()
-                if parent == None:
+                if parent is None:
                     self.redirect('/admin')
                 feed = Feed(name=name, feed=url, category=parent)
                 feed.put()
--- index.py
+++ index.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 import os
+from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from models.data import *
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
+    def get(self, *arg):
         path = os.path.join(os.path.dirname(__file__), 'views/index.html')
-        categories = Category.all().fetch(1000)
-        feeds = Feed.all().fetch(1000)
-        planets = Planet.all().fetch(1000)
+        categories = memcache.get('categories')
+        if categories is None:
+            categories = Category.all().fetch(1000)
+            memcache.set(key='categories', value=categories, time=3600)
+        feeds = memcache.get('feeds')
+        if feeds is None:
+            feeds = Feed.all().fetch(1000)
+            memcache.set(key='feeds', value=feeds, time=3600)
+        planets = memcache.get('planets')
+        if planets is None:
+            planets = Planet.all().fetch(1000)
+            memcache.set(key='planets', value=planets, time=3600)
         vars = {
                 'title': 'Planet Linux of Taiwan',
                 'categories': categories,
@@ -20,7 +30,11 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, vars))
 
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [
+                                         ('/', MainPage),
+                                         ('/join', MainPage),
+                                         ('/category/(.*)', MainPage),
+                                         ],
                                      debug=True)
 
 def main():
