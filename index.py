75cfb8c12c4b98ea2bac330904422da2ab9f0ea9
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2009-09-28 23:46:34 +0800

    Add recaptcha on join request.

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from google.appengine.ext.db import BadValueError
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from models.data import *
+from recaptcha.client import captcha
 
 class MainPage(webapp.RequestHandler):
     def get(self, *arg):
@@ -45,46 +46,64 @@ class Join(webapp.RequestHandler):
         if planets is None:
             planets = Planet.all().fetch(1000)
             memcache.set(key='planets', value=planets, time=3600)
+        chtml = captcha.displayhtml(
+                public_key = "PUBLIC_KEY",
+                use_ssl = False,
+                error = None)
         vars = {
                 'title': 'Join - Planet Linux of Taiwan',
                 'categories': categories,
                 'feeds': feeds,
                 'planets': planets,
+                'captchahtml': chtml,
                 }
         self.response.out.write(template.render(path, vars))
     def post(self):
-        try:
-            name = self.request.get('name')
-            feed = self.request.get('feed')
-            avatar = self.request.get('avatar')
-            image = images.Image(avatar)
-            image.im_feeling_lucky()
-            if image.width > 100 or image.height > 100:
-                image.resize(width=100, height=100)
-            join = JoinRequest(name=name, feed=feed, avatar=image.execute_transforms(output_encoding=images.PNG))
-            join.put()
-            path = os.path.join(os.path.dirname(__file__), 'views/result.html')
-            categories = memcache.get('categories')
-            if categories is None:
-                categories = Category.all().fetch(1000)
-                memcache.set(key='categories', value=categories, time=3600)
-            feeds = memcache.get('feeds')
-            if feeds is None:
-                feeds = Feed.all().fetch(1000)
-                memcache.set(key='feeds', value=feeds, time=3600)
-            planets = memcache.get('planets')
-            if planets is None:
-                planets = Planet.all().fetch(1000)
-                memcache.set(key='planets', value=planets, time=3600)
-            vars = {
-                    'title': 'Join - Planet Linux of Taiwan',
-                    'categories': categories,
-                    'feeds': feeds,
-                    'planets': planets,
-                    'message': 'Done!',
-                    }
-            self.response.out.write(template.render(path, vars))
-        except BadValueError:
+        challenge = self.request.get('recaptcha_challenge_field')
+        response  = self.request.get('recaptcha_response_field')
+        remoteip  = os.environ.get('REMOTE_ADDR')
+        cResponse = captcha.submit(
+                challenge,
+                response,
+                "PRIVATE_KEY",
+                remoteip)
+
+        if cResponse.is_valid:
+            try:
+                name = self.request.get('name')
+                feed = self.request.get('feed')
+                avatar = self.request.get('avatar')
+                image = images.Image(avatar)
+                image.im_feeling_lucky()
+                if image.width > 100 or image.height > 100:
+                    image.resize(width=100, height=100)
+                join = JoinRequest(name=name, feed=feed, avatar=image.execute_transforms(output_encoding=images.PNG))
+                join.put()
+                path = os.path.join(os.path.dirname(__file__), 'views/result.html')
+                categories = memcache.get('categories')
+                if categories is None:
+                    categories = Category.all().fetch(1000)
+                    memcache.set(key='categories', value=categories, time=3600)
+                feeds = memcache.get('feeds')
+                if feeds is None:
+                    feeds = Feed.all().fetch(1000)
+                    memcache.set(key='feeds', value=feeds, time=3600)
+                planets = memcache.get('planets')
+                if planets is None:
+                    planets = Planet.all().fetch(1000)
+                    memcache.set(key='planets', value=planets, time=3600)
+                vars = {
+                        'title': 'Join - Planet Linux of Taiwan',
+                        'categories': categories,
+                        'feeds': feeds,
+                        'planets': planets,
+                        'message': 'Done!',
+                        }
+                self.response.out.write(template.render(path, vars))
+            except BadValueError:
+                self.redirect('/join')
+        else:
+            error = cResponse.error_code
             self.redirect('/join')
 
 class Avatar(webapp.RequestHandler):
